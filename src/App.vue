197ecd26def9f3ffc35948e4966a092ab02aca07
JoaoIsabel <64496164+JoaoIsabel@users.noreply.github.com>
2022-04-06 11:31:54 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SimpleCalculator />
+  </div>
+</template>
+
+<script>
+import SimpleCalculator from './components/SimpleCalculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimpleCalculator
+  }
+}
+</script>
+
+<style>
+
+</style>
